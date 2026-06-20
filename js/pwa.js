35fe00933c087c7ed2c94a7dfb6dba72a29bf706
PolimataStudio <polimatastudio.comercial@gmail.com>
2026-06-20 20:16:49 -0300

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -1,6 +1,6 @@
 export function initPWA() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/pscomercial/service-worker.js')
       .then(reg => console.log('Service Worker registrado com sucesso.', reg))
       .catch(err => console.warn('Falha ao registrar Service Worker.', err));
   }
